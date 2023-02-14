71f0a991e7fca9f76f088553cb4084d66021cb3a
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2023-02-14 13:02:22 +0000

    added spring boot starter

---
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/CrownCourtContributionApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.contribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownCourtContributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrownCourtContributionApplication.class, args);
+	}
+
+}
---
+++ crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/CrownCourtContributionApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.contribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrownCourtContributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
