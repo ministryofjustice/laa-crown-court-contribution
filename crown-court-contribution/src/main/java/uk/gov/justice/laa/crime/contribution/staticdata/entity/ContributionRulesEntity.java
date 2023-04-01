e4b3d8a67f261c52a1852e1790b9ce2566511766
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2023-04-02 00:19:14 +0100

    LCAM-391-contribution rules static table

---
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/staticdata/entity/ContributionRulesEntity.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.crime.contribution.staticdata.entity;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "contribution_rules", schema = "crown_court_contribution")
+public class ContributionRulesEntity {
+    @Id
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "CASE_TYPE")
+    private String caseType;
+
+    @Column(name = "MCOO_OUTCOME")
+    private String magistratesCourtOutcome;
+
+    @Column(name = "CCOO_OUTCOME")
+    private String crownCourtOutcome;
+
+    @Column(name = "VARIATION")
+    private String variation;
+
+    @Column(name = "VARIATION_RULE")
+    private String variationRule;
+
+}
