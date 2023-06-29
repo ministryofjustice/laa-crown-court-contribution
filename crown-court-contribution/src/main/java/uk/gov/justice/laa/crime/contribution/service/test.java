50ba19441340ebbae5ef53e2b34b4cce192cd089
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-06-29 21:43:25 +0100

    LCAM-400: Business logic for compare contribution

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/test.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.laa.crime.contribution.service;
-
-import uk.gov.justice.laa.crime.contribution.staticdata.enums.CaseType;
-
-public class test {
-    public static void main(String args[]){
-        System.out.println(CaseType.APPEAL_CC.getCaseTypeString());
-    }
-}
