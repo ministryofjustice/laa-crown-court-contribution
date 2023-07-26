882b58ef58edbf507e58950c86f0f362b39087bd
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2023-07-26 12:26:03 +0100

    LCAM-815| removed auth server config and added cognito scope

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/config/ResourceServerConfiguration.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/config/ResourceServerConfiguration.java
@@ -3,52 +3,48 @@ package uk.gov.justice.laa.crime.contribution.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Order(1)
 @EnableWebSecurity
 public class ResourceServerConfiguration {
 
-    private static final String API_PATH = "/api/**";
+    public static final String SCOPE_CCC_STANDARD = "SCOPE_ccc/standard";
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
-        http
-                .sessionManagement(sessionManagement ->
-                        sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/oauth2/**")
-                        .permitAll()
-                        .requestMatchers("/open-api/**")
-                        .permitAll()
-                        .requestMatchers("/actuator/**")
-                        .permitAll()
-                        .requestMatchers("/error")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.GET, API_PATH)
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST, API_PATH)
-                        .permitAll()
-                        .requestMatchers(HttpMethod.PUT, API_PATH)
-                        .permitAll()
-                        .requestMatchers(HttpMethod.DELETE, API_PATH)
-                        .permitAll()
-                        .requestMatchers(HttpMethod.PATCH, API_PATH)
-                        .permitAll()
-                        .anyRequest().anonymous()
-                ).csrf().disable();
+    protected BearerTokenAuthenticationEntryPoint bearerTokenAuthenticationEntryPoint() {
+        BearerTokenAuthenticationEntryPoint bearerTokenAuthenticationEntryPoint = new BearerTokenAuthenticationEntryPoint();
+        bearerTokenAuthenticationEntryPoint.setRealmName("Crown Court Contribution API");
+        return bearerTokenAuthenticationEntryPoint;
+    }
 
+    @Bean
+    public AccessDeniedHandler bearerTokenAccessDeniedHandler() {
+        return new BearerTokenAccessDeniedHandler();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/open-api/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/**").hasAuthority(SCOPE_CCC_STANDARD)
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .accessDeniedHandler(bearerTokenAccessDeniedHandler())
+                        .authenticationEntryPoint(bearerTokenAuthenticationEntryPoint())
+                        .jwt(withDefaults()));
         return http.build();
     }
+
 }
