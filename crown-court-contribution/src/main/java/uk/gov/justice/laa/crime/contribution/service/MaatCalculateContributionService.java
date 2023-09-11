28d0bcb2bc8aa001717179854826717a6a1b007a
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2023-09-11 09:16:53 +0100

    LCAM-930|Update response with processActivity flag

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/MaatCalculateContributionService.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/MaatCalculateContributionService.java
@@ -9,7 +9,8 @@ import uk.gov.justice.laa.crime.contribution.builder.MaatCalculateContributionRe
 import uk.gov.justice.laa.crime.contribution.builder.UpdateContributionRequestMapper;
 import uk.gov.justice.laa.crime.contribution.common.Constants;
 import uk.gov.justice.laa.crime.contribution.dto.*;
-import uk.gov.justice.laa.crime.contribution.model.*;
+import uk.gov.justice.laa.crime.contribution.model.ApiCalculateContributionRequest;
+import uk.gov.justice.laa.crime.contribution.model.Contribution;
 import uk.gov.justice.laa.crime.contribution.model.common.Assessment;
 import uk.gov.justice.laa.crime.contribution.model.maat_api.*;
 import uk.gov.justice.laa.crime.contribution.staticdata.enums.*;
@@ -106,19 +107,23 @@ public class MaatCalculateContributionService {
 
         Contribution currentContribution = getCurrentContribution(calculateContributionDTO, laaTransactionId);
 
-        verifyAndCreateContribs(calculateContributionDTO, laaTransactionId, isReassessment, repOrderDTO,
+        Contribution createdContribution = verifyAndCreateContribs(calculateContributionDTO, laaTransactionId, isReassessment, repOrderDTO,
                 response, currentContribution);
 
-        verifyAndUpdateContribution(calculateContributionDTO, laaTransactionId, response, currentContribution);
+        requestEarlyTransfer(calculateContributionDTO, laaTransactionId, response, currentContribution);
 
-        //Call Matrix Activity and make sure corr_id is updated with the Correspondence ID
+        if (contributionResponseDTO.getTemplate() != null && createdContribution != null) {
+            response.setProcessActivity(true);
+        }
         return response;
     }
 
-    private void verifyAndCreateContribs(CalculateContributionDTO calculateContributionDTO, String laaTransactionId,
-                                         boolean isReassessment, RepOrderDTO repOrderDTO,
-                                         MaatCalculateContributionResponse response,
-                                         Contribution currentContribution) {
+    public Contribution verifyAndCreateContribs(final CalculateContributionDTO calculateContributionDTO,
+                                         final String laaTransactionId,
+                                         final boolean isReassessment,
+                                         final RepOrderDTO repOrderDTO,
+                                         final MaatCalculateContributionResponse response,
+                                         final Contribution currentContribution) {
         TransferStatus currentTransferStatus = null;
         Integer currentContributionFileId = null;
 
@@ -140,13 +145,17 @@ public class MaatCalculateContributionService {
                 maatCourtDataService.updateContribution(updateContributionRequest, laaTransactionId);
             }
             //Revisit the createContribs logic - do we need to change the input?
-            createContribs(calculateContributionDTO, laaTransactionId);
+            return createContribs(calculateContributionDTO, laaTransactionId);
         } else if (isCreateContributionRequired(calculateContributionDTO, isReassessment, repOrderDTO, currentTransferStatus)) {
-            createContribs(calculateContributionDTO, laaTransactionId);
+            return createContribs(calculateContributionDTO, laaTransactionId);
         }
+        return null;
     }
 
-    private void verifyAndUpdateContribution(CalculateContributionDTO calculateContributionDTO, String laaTransactionId, MaatCalculateContributionResponse response, Contribution currentContribution) {
+    public void requestEarlyTransfer(final CalculateContributionDTO calculateContributionDTO,
+                                     final String laaTransactionId,
+                                     final MaatCalculateContributionResponse response,
+                                     final Contribution currentContribution) {
         Contribution latestSentContribution = maatCourtDataService.findLatestSentContribution(calculateContributionDTO.getRepId(), laaTransactionId);
         if (isEarlyTransferRequired(calculateContributionDTO, laaTransactionId, response, latestSentContribution) && currentContribution != null) {
             maatCourtDataService.updateContribution(new UpdateContributionRequest()
@@ -156,7 +165,7 @@ public class MaatCalculateContributionService {
         }
     }
 
-    public Contribution getCurrentContribution(CalculateContributionDTO calculateContributionDTO, final String laaTransactionId){
+    public Contribution getCurrentContribution(final CalculateContributionDTO calculateContributionDTO, final String laaTransactionId) {
         final Integer contributionId = calculateContributionDTO.getId();
         List<Contribution> contributionsList = new ArrayList<>();
         if (contributionId != null) {
@@ -207,7 +216,7 @@ public class MaatCalculateContributionService {
                 calculateContributionDTO.getMagCourtOutcome(), crownCourtOutcome);
 
         BigDecimal annualDisposableIncome = calculateAnnualDisposableIncome(calculateContributionDTO, laaTransactionId, crownCourtOutcome, isContributionRuleApplicable);
-        Integer totalMonths = Constants.N.equals(contributionResponseDTO.getCalcContribs()) ? 0:null;
+        Integer totalMonths = Constants.N.equals(contributionResponseDTO.getCalcContribs()) ? 0 : null;
 
         ApiCalculateContributionRequest apiCalculateContributionRequest = calculateContributionRequestMapper.map(contributionCalcParametersDTO,
                 annualDisposableIncome, isUpliftApplied(calculateContributionDTO, contributionResponseDTO),
