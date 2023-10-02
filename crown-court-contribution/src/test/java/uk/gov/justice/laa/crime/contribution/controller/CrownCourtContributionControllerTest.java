9d5094f96f894d22712442808559416b6c11cbf6
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-10-02 16:24:58 +0100

    Test scenarios for new controller endpoint

--- crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/controller/CrownCourtContributionControllerTest.java
+++ crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/controller/CrownCourtContributionControllerTest.java
@@ -14,6 +14,7 @@ import uk.gov.justice.laa.crime.commons.exception.APIClientException;
 import uk.gov.justice.laa.crime.contribution.data.builder.TestModelDataBuilder;
 import uk.gov.justice.laa.crime.contribution.dto.CalculateContributionDTO;
 import uk.gov.justice.laa.crime.contribution.exeption.ValidationException;
+import uk.gov.justice.laa.crime.contribution.model.ApiContributionTransferRequest;
 import uk.gov.justice.laa.crime.contribution.model.ApiMaatCalculateContributionRequest;
 import uk.gov.justice.laa.crime.contribution.model.ApiMaatCalculateContributionResponse;
 import uk.gov.justice.laa.crime.contribution.model.common.ApiContributionSummary;
@@ -25,8 +26,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.justice.laa.crime.contribution.util.RequestBuilderUtils.buildRequestGivenContent;
@@ -36,10 +37,12 @@ import static uk.gov.justice.laa.crime.contribution.util.RequestBuilderUtils.bui
 @WebMvcTest(CrownCourtContributionController.class)
 class CrownCourtContributionControllerTest {
 
-    private static final String ENDPOINT_URL = "/api/internal/v1/contribution/calculate-contribution";
+    private static final String BASE_URL = "/api/internal/v1/contribution";
+    private static final String ENDPOINT_URL = BASE_URL + "/calculate-contribution";
+    private static final String GET_CONTRIBUTION_SUMMARIES_ENDPOINT_URL =
+            BASE_URL + TestModelDataBuilder.REP_ID + "/summaries";
+    private static final String REQUEST_CONTRIBUTIONS_TRANSFER_ENDPOINT_URL = BASE_URL + "/request-transfer";
 
-    private static final String GET_CONTRIBUTION_SUMMARIES_ENDPOINT_URL = "/api/internal/v1/contribution/" + TestModelDataBuilder.REP_ID
-            + "/summaries";
 
     @Autowired
     private MockMvc mvc;
@@ -58,10 +61,13 @@ class CrownCourtContributionControllerTest {
 
     @Test
     void givenValidRequest_whenCalculateAppealContributionIsInvoked_thenOkResponse() throws Exception {
-        ApiMaatCalculateContributionRequest appealContributionRequest = TestModelDataBuilder.buildAppealContributionRequest();
+        ApiMaatCalculateContributionRequest appealContributionRequest =
+                TestModelDataBuilder.buildAppealContributionRequest();
+
         String requestData = objectMapper.writeValueAsString(appealContributionRequest);
 
-        when(calculateContributionValidator.validate(any(ApiMaatCalculateContributionRequest.class))).thenReturn(Optional.empty());
+        when(calculateContributionValidator.validate(any(ApiMaatCalculateContributionRequest.class)))
+                .thenReturn(Optional.empty());
 
         when(maatCalculateContributionService.calculateContribution(any(CalculateContributionDTO.class), any()))
                 .thenReturn(new ApiMaatCalculateContributionResponse());
@@ -73,7 +79,9 @@ class CrownCourtContributionControllerTest {
 
     @Test
     void givenInvalidRequest_whenCalculateAppealContributionIsInvoked_thenBadRequestResponse() throws Exception {
-        ApiMaatCalculateContributionRequest appealContributionRequest = TestModelDataBuilder.buildAppealContributionRequest();
+        ApiMaatCalculateContributionRequest appealContributionRequest =
+                TestModelDataBuilder.buildAppealContributionRequest();
+
         String requestData = objectMapper.writeValueAsString(appealContributionRequest);
 
         when(calculateContributionValidator.validate(any(ApiMaatCalculateContributionRequest.class)))
@@ -86,11 +94,14 @@ class CrownCourtContributionControllerTest {
 
     @Test
     void givenClientApiException_whenCalculateAppealContributionIsInvoked_thenInternalServerErrorResponse() throws Exception {
-        ApiMaatCalculateContributionRequest appealContributionRequest = TestModelDataBuilder.buildAppealContributionRequest();
+        ApiMaatCalculateContributionRequest appealContributionRequest =
+                TestModelDataBuilder.buildAppealContributionRequest();
+
         String requestData = objectMapper.writeValueAsString(appealContributionRequest);
 
         when(calculateContributionValidator.validate(any(ApiMaatCalculateContributionRequest.class)))
                 .thenReturn(Optional.empty());
+
         when(maatCalculateContributionService.calculateContribution(any(CalculateContributionDTO.class), any()))
                 .thenThrow(new APIClientException("Test api client exception"));
 
@@ -117,6 +128,29 @@ class CrownCourtContributionControllerTest {
         mvc.perform(buildRequestGivenContent(HttpMethod.GET, "", GET_CONTRIBUTION_SUMMARIES_ENDPOINT_URL, false))
                 .andExpect(status().isInternalServerError())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void givenValidRequest_whenRequestTransferIsInvoked_thenOkResponse() throws Exception {
+        ApiContributionTransferRequest request = new ApiContributionTransferRequest()
+                .withContributionId(1000)
+                .withUserModified("mock-u");
+
+        String body = objectMapper.writeValueAsString(request);
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, body, REQUEST_CONTRIBUTIONS_TRANSFER_ENDPOINT_URL, true))
+                .andExpect(status().isOk());
+
+        verify(contributionService).requestTransfer(any(ApiContributionTransferRequest.class), anyString());
+    }
+
+    @Test
+    void givenInValidRequest_whenRequestTransferIsInvoked_thenBadRequestResponse() throws Exception {
+        ApiContributionTransferRequest request = new ApiContributionTransferRequest()
+                .withContributionId(1000);
+
+        String body = objectMapper.writeValueAsString(request);
+        mvc.perform(buildRequestGivenContent(HttpMethod.POST, body, REQUEST_CONTRIBUTIONS_TRANSFER_ENDPOINT_URL, true))
+                .andExpect(status().isBadRequest());
     }
 
 }
