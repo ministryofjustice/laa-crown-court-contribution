fe01bd83c2b2ff49a37312c5bd5f5156a1330a5f
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-10-17 18:01:17 +0100

    LCAM-985: Upgraded the laa-crime-commons version and added micrometer tracing configuration

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/exeption/CCCExceptionHandler.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/exeption/CCCExceptionHandler.java
@@ -1,30 +1,35 @@
 package uk.gov.justice.laa.crime.contribution.exeption;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import uk.gov.justice.laa.crime.commons.exception.APIClientException;
+import uk.gov.justice.laa.crime.commons.tracing.TraceIdHandler;
 import uk.gov.justice.laa.crime.contribution.dto.ErrorDTO;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class CCCExceptionHandler {
 
-    private static ResponseEntity<ErrorDTO> buildErrorResponse(HttpStatus status, String message) {
-        return new ResponseEntity<>(ErrorDTO.builder().code(status.toString()).message(message).build(), status);
+    private final TraceIdHandler traceIdHandler;
+
+    private static ResponseEntity<ErrorDTO> buildErrorResponse(HttpStatus status, String message, String traceId) {
+        return new ResponseEntity<>(ErrorDTO.builder().traceId(traceId).code(status.toString()).message(message).build(), status);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorDTO> handleValidationException(ValidationException exception) {
         log.error("Validation exception: {}", exception.getMessage());
-        return buildErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage(), traceIdHandler.getTraceId() );
     }
 
     @ExceptionHandler(APIClientException.class)
     public ResponseEntity<ErrorDTO> handleApiClientException(APIClientException exception) {
         log.error("API client exception: {}", exception.getMessage());
-        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), traceIdHandler.getTraceId());
     }
 }
