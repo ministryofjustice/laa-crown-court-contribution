ce62e93414f7b40c397e2882f0e708c87fb7f760
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-02-15 14:48:08 +0000

    LCAM-1223|Remove the redundant logic and tidy up tests

--- crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/service/MaatCourtDataServiceTest.java
+++ crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/service/MaatCourtDataServiceTest.java
@@ -7,8 +7,6 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import uk.gov.justice.laa.crime.commons.client.RestAPIClient;
@@ -31,9 +29,7 @@ import uk.gov.justice.laa.crime.enums.CrownCourtAppealOutcome;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.anyInt;
@@ -41,7 +37,6 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MaatCourtDataServiceTest {
@@ -138,28 +133,6 @@ class MaatCourtDataServiceTest {
                 anyString(),
                 anyMap()
         );
-    }
-
-    @Test
-    void givenValidRepId_whenContributionCountIsInvoked_thenResponseIsReturned() {
-        maatCourtDataService.getContributionCount(TEST_REP_ID);
-        verify(maatCourtDataClient).head(configuration.getMaatApi().getContributionEndpoints().getGetContributionCountUrl(),
-                Map.of(), TEST_REP_ID);
-    }
-
-    @Test
-    void givenValidRepId_whenContributionCountIsInvoked_thenContentLengthOfResponseIsReturned() {
-        ResponseEntity<Void> responseEntity = ResponseEntity.ok().header(HttpHeaders.CONTENT_LENGTH, "23").build();
-        when(maatCourtDataClient.head(configuration.getMaatApi().getContributionEndpoints().getGetContributionCountUrl(),
-                Map.of(), TEST_REP_ID))
-                .thenReturn(responseEntity);
-
-        long actualContributionCount = maatCourtDataService.getContributionCount(TEST_REP_ID);
-
-        verify(maatCourtDataClient).head(configuration.getMaatApi().getContributionEndpoints().getGetContributionCountUrl(),
-                Map.of(), TEST_REP_ID);
-
-        assertEquals(23, actualContributionCount);
     }
 
     @Test
