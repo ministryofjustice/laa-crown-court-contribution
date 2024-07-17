666fcd639b4eef94ada7094c86393f2a3cc7e1fa
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2024-07-17 10:29:13 +0100

    added more logs

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/builder/ContributionResponseDTOBuilder.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/builder/ContributionResponseDTOBuilder.java
@@ -2,15 +2,24 @@ package uk.gov.justice.laa.crime.contribution.builder;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.crime.contribution.dto.ContributionResponseDTO;
 import uk.gov.justice.laa.crime.contribution.projection.CorrespondenceRuleAndTemplateInfo;
 
+@Slf4j
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ContributionResponseDTOBuilder {
 
     public static ContributionResponseDTO build(final CorrespondenceRuleAndTemplateInfo request) {
+
+        log.info("id--"+ request.getId());
+        log.info("Calc contribs--"+ request.getCalcContribs());
+        log.info(" Description--"+ request.getDescription());
+        log.info("CotyCorrespondenceType--"+ request.getCotyCorrespondenceType());
+        log.info("UpliftCoteId--"+ request.getUpliftCoteId());
+        log.info("ReassessmentCoteId--"+ request.getReassessmentCoteId());
         ContributionResponseDTO.ContributionResponseDTOBuilder builder = ContributionResponseDTO.builder()
                 .id(request.getId())
                 .calcContribution(request.getCalcContribs())
