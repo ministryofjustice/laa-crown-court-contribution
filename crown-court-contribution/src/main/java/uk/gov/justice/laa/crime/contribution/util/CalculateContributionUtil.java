1a2e7f376121d3081ffb93f07cf9674de534b834
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2024-07-31 15:06:10 +0100

    contribution

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/util/CalculateContributionUtil.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/util/CalculateContributionUtil.java
@@ -13,9 +13,9 @@ public class CalculateContributionUtil {
     public BigDecimal calculateMonthlyContribution(final BigDecimal annualDisposableIncome, final BigDecimal disposableIncomePercent, final BigDecimal minimumMonthlyAmount) {
         if(checkNull(annualDisposableIncome, disposableIncomePercent, minimumMonthlyAmount))
             return null;
-        BigDecimal monthlyContributionsCalc = annualDisposableIncome.divide(BigDecimal.valueOf(12), RoundingMode.FLOOR)
+        BigDecimal monthlyContributionsCalc = new BigDecimal(Math.floor((annualDisposableIncome.divide(BigDecimal.valueOf(12), RoundingMode.FLOOR)
                 .multiply(disposableIncomePercent)
-                .divide(BigDecimal.valueOf(100), RoundingMode.FLOOR);
+                .divide(BigDecimal.valueOf(100), RoundingMode.FLOOR)).doubleValue())).setScale(2);
         BigDecimal monthlyContribution = (monthlyContributionsCalc.compareTo(BigDecimal.ZERO) > 0) ? monthlyContributionsCalc : BigDecimal.ZERO;
         if (monthlyContribution.compareTo(minimumMonthlyAmount) < 0) {
             return BigDecimal.ZERO;
@@ -26,7 +26,7 @@ public class CalculateContributionUtil {
     public BigDecimal calculateUpfrontContributions(final BigDecimal monthlyContributions, final BigDecimal contributionCap, final Integer upfrontTotalMonths) {
         if(checkNull(monthlyContributions, contributionCap) || upfrontTotalMonths == null)
             return null;
-        BigDecimal upfrontContribution = monthlyContributions.multiply(BigDecimal.valueOf(upfrontTotalMonths));
+        BigDecimal upfrontContribution = monthlyContributions.multiply(BigDecimal.valueOf(upfrontTotalMonths)).setScale(2, RoundingMode.FLOOR);
         if (upfrontContribution.compareTo(contributionCap) < 0) {
             return upfrontContribution;
         } else return contributionCap;
@@ -35,9 +35,10 @@ public class CalculateContributionUtil {
     public BigDecimal calculateUpliftedMonthlyAmount(final BigDecimal annualDisposableIncome, final BigDecimal upliftedIncomePercent, final BigDecimal minUpliftedMonthlyAmount) {
         if(checkNull(annualDisposableIncome, upliftedIncomePercent, minUpliftedMonthlyAmount))
             return null;
-        BigDecimal monthlyContributionsCalc = annualDisposableIncome.divide(BigDecimal.valueOf(12), RoundingMode.FLOOR)
+        BigDecimal monthlyContributionsCalc = new BigDecimal(Math.floor((annualDisposableIncome.divide(BigDecimal.valueOf(12), RoundingMode.FLOOR)
                 .multiply(upliftedIncomePercent)
-                .divide(BigDecimal.valueOf(100), RoundingMode.FLOOR);
+                .divide(BigDecimal.valueOf(100), RoundingMode.FLOOR).doubleValue()))).setScale(2);
+        monthlyContributionsCalc.setScale(1, RoundingMode.FLOOR);
         if (monthlyContributionsCalc.compareTo(minUpliftedMonthlyAmount) > 0) {
             return monthlyContributionsCalc;
         }
