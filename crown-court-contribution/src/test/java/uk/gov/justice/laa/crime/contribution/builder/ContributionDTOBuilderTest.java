6e3de65cde55d2faebe18c508561a55a0d7599bc
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-08-06 14:21:23 +0100

    added unit test for CalculateContributionDTOBuilder

--- crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/builder/ContributionDTOBuilderTest.java
+++ crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/builder/ContributionDTOBuilderTest.java
@@ -27,7 +27,7 @@ class ContributionDTOBuilderTest {
     }
 
     @Test
-    void givenAValidApiMaatCalculateContributionRequestShouldBuildAPopulatedCalculateContributionDTO() {
+    void givenAValidRequestWithoutContributions_whenBuildIsInvoked_thenACalculateContributionDTOWithZeroContributionsIsCreated() {
         ApiMaatCalculateContributionRequest contributionRequest = TestModelDataBuilder.buildAppealContributionRequest();
 
         CalculateContributionDTO actualContributionDTO = ContributionDTOBuilder.build(contributionRequest);
@@ -39,6 +39,39 @@ class ContributionDTOBuilderTest {
         softly.assertThat(actualContributionDTO.getContributionCap()).isNull();
         softly.assertThat(actualContributionDTO.getMonthlyContributions()).isEqualTo(BigDecimal.ZERO);
         softly.assertThat(actualContributionDTO.getUpfrontContributions()).isEqualTo(BigDecimal.ZERO);
+        softly.assertThat(actualContributionDTO.getDateUpliftApplied()).isNull();
+        softly.assertThat(actualContributionDTO.getDateUpliftRemoved()).isNull();
+        softly.assertThat(actualContributionDTO.getCaseType()).isEqualTo(CaseType.APPEAL_CC);
+        softly.assertThat(actualContributionDTO.getAssessments()).hasSize(1);
+        softly.assertThat(actualContributionDTO.getAppealType()).isEqualTo(AppealType.ACS);
+        softly.assertThat(actualContributionDTO.getLastOutcome().getOutcome()).isEqualTo(CrownCourtAppealOutcome.SUCCESSFUL);
+        softly.assertThat(actualContributionDTO.getLastOutcome().getDateSet()).isEqualTo(LocalDateTime.parse("2022-01-01T00:00:00"));
+        softly.assertThat(actualContributionDTO.getRemoveContribs()).isNull();
+        softly.assertThat(actualContributionDTO.getCommittalDate()).isNull();
+        softly.assertThat(actualContributionDTO.getMagCourtOutcome()).isNull();
+        softly.assertThat(actualContributionDTO.getCrownCourtOutcomeList()).isEmpty();
+        softly.assertThat(actualContributionDTO.getDisposableIncomeAfterCrownHardship()).isNull();
+        softly.assertThat(actualContributionDTO.getDisposableIncomeAfterMagHardship()).isNull();
+        softly.assertThat(actualContributionDTO.getTotalAnnualDisposableIncome()).isNull();
+
+        softly.assertAll();
+    }
+
+    @Test
+    void givenAValidRequestWithContributions_whenBuildIsInvoked_thenACalculateContributionDTOWithContributionsIsCreated() {
+        ApiMaatCalculateContributionRequest contributionRequest = TestModelDataBuilder.buildAppealContributionRequest();
+        contributionRequest.setMonthlyContributions(BigDecimal.valueOf(123));
+        contributionRequest.setUpfrontContributions(BigDecimal.valueOf(123));
+
+        CalculateContributionDTO actualContributionDTO = ContributionDTOBuilder.build(contributionRequest);
+
+        softly.assertThat(actualContributionDTO.getContributionId()).isNull();
+        softly.assertThat(actualContributionDTO.getApplId()).isEqualTo(999);
+        softly.assertThat(actualContributionDTO.getRepId()).isEqualTo(999);
+        softly.assertThat(actualContributionDTO.getEffectiveDate()).isNull();
+        softly.assertThat(actualContributionDTO.getContributionCap()).isNull();
+        softly.assertThat(actualContributionDTO.getMonthlyContributions()).isEqualTo(BigDecimal.valueOf(123));
+        softly.assertThat(actualContributionDTO.getUpfrontContributions()).isEqualTo(BigDecimal.valueOf(123));
         softly.assertThat(actualContributionDTO.getDateUpliftApplied()).isNull();
         softly.assertThat(actualContributionDTO.getDateUpliftRemoved()).isNull();
         softly.assertThat(actualContributionDTO.getCaseType()).isEqualTo(CaseType.APPEAL_CC);
