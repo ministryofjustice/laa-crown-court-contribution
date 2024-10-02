c9dd37daa17e77447c24eacf8203267585441ca3
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2024-10-02 09:16:33 +0100

    reworked appeal contribution calculation logic

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/AppealContributionService.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/AppealContributionService.java
@@ -4,17 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.crime.common.model.common.ApiCrownCourtOutcome;
 import uk.gov.justice.laa.crime.common.model.contribution.ApiMaatCalculateContributionResponse;
 import uk.gov.justice.laa.crime.common.model.contribution.ApiAssessment;
 import uk.gov.justice.laa.crime.common.model.contribution.maat_api.CreateContributionRequest;
-import uk.gov.justice.laa.crime.common.model.contribution.maat_api.GetContributionAmountRequest;
 import uk.gov.justice.laa.crime.contribution.builder.CreateContributionRequestMapper;
-import uk.gov.justice.laa.crime.contribution.builder.GetContributionAmountRequestMapper;
 import uk.gov.justice.laa.crime.contribution.builder.MaatCalculateContributionResponseMapper;
 import uk.gov.justice.laa.crime.contribution.dto.CalculateContributionDTO;
 import uk.gov.justice.laa.crime.contribution.model.Contribution;
 import uk.gov.justice.laa.crime.contribution.staticdata.enums.AppealContributionAmount;
 import uk.gov.justice.laa.crime.enums.AssessmentResult;
+import uk.gov.justice.laa.crime.enums.CrownCourtOutcomeType;
 import uk.gov.justice.laa.crime.enums.CurrentStatus;
 
 import java.math.BigDecimal;
@@ -26,7 +26,6 @@ import java.util.List;
 public class AppealContributionService {
 
     private final MaatCourtDataService maatCourtDataService;
-    private final GetContributionAmountRequestMapper getContributionAmountRequestMapper;
     private final CreateContributionRequestMapper createContributionRequestMapper;
     private final MaatCalculateContributionResponseMapper maatCalculateContributionResponseMapper;
 
@@ -42,37 +41,34 @@ public class AppealContributionService {
     public ApiMaatCalculateContributionResponse calculateAppealContribution(
             CalculateContributionDTO calculateContributionDTO) {
         AssessmentResult assessmentResult = determineAssessmentResult(calculateContributionDTO.getAssessments());
+        ApiCrownCourtOutcome latestAppealOutcome = calculateContributionDTO.getCrownCourtOutcomeList()
+                .stream()
+                .filter(outcome -> CrownCourtOutcomeType.APPEAL.getType().equals(outcome.getOutcome().getType()))
+                .reduce((first, second) -> second)
+                .orElse(null);
 
-        // TODO: Need to amend this mapper as we are no longer passing through lastOutcome field in DTO
-        // TODO: Also do we even need this object at all if we are only passing members of it later in method?
-        GetContributionAmountRequest getContributionAmountRequest =
-                getContributionAmountRequestMapper.map(calculateContributionDTO, assessmentResult);
         BigDecimal appealContributionAmount = null;
-
-        // TODO: Replace this conditional with check for non empty list of outcomes
-        // TODO: Also change this if conditional to encompass the rest of the logic in this method
-        if (getContributionAmountRequest.getOutcome() != null &&
-                getContributionAmountRequest.getAssessmentResult() != null) {
+        if (latestAppealOutcome != null) {
             appealContributionAmount = AppealContributionAmount.calculate(
-                            getContributionAmountRequest.getAppealType(), getContributionAmountRequest.getOutcome(),
-                            getContributionAmountRequest.getAssessmentResult()
+                            calculateContributionDTO.getAppealType(), latestAppealOutcome.getOutcome(),
+                            assessmentResult
                     )
                     .getContributionAmount();
-        }
 
-        Integer repId = calculateContributionDTO.getRepId();
-        List<Contribution> currContributionList = maatCourtDataService.findContribution(repId, true);
-        if (CollectionUtils.isNotEmpty(currContributionList)) {
-            Contribution currContribution = currContributionList.get(0);
-            if (currContribution.getUpfrontContributions() == null
-                    || (appealContributionAmount == null
-                    || currContribution.getUpfrontContributions().compareTo(appealContributionAmount) != 0)) {
-                CreateContributionRequest createContributionRequest =
-                        createContributionRequestMapper.map(calculateContributionDTO, appealContributionAmount);
-                Contribution newContribution = maatCourtDataService.createContribution(createContributionRequest);
-                return maatCalculateContributionResponseMapper.map(newContribution);
+            Integer repId = calculateContributionDTO.getRepId();
+            List<Contribution> currContributionList = maatCourtDataService.findContribution(repId, true);
+            if (CollectionUtils.isNotEmpty(currContributionList)) {
+                Contribution currContribution = currContributionList.get(0);
+                if (currContribution.getUpfrontContributions() == null
+                        || (appealContributionAmount == null
+                        || currContribution.getUpfrontContributions().compareTo(appealContributionAmount) != 0)) {
+                    CreateContributionRequest createContributionRequest =
+                            createContributionRequestMapper.map(calculateContributionDTO, appealContributionAmount);
+                    Contribution newContribution = maatCourtDataService.createContribution(createContributionRequest);
+                    return maatCalculateContributionResponseMapper.map(newContribution);
+                }
+                return maatCalculateContributionResponseMapper.map(currContribution);
             }
-            return maatCalculateContributionResponseMapper.map(currContribution);
         }
         return new ApiMaatCalculateContributionResponse();
     }
