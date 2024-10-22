da95023e9ba18c3143d1e78a6a7d1a12f7405731
Muthu Subramanian <muthu.subramanian@digital.justice.gov.uk>
2024-10-22 13:21:57 +0100

    Added log statements to log the request

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/MaatCourtDataService.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/MaatCourtDataService.java
@@ -29,17 +29,19 @@ public class MaatCourtDataService {
     private static final String RESPONSE_STRING = "Response from Court Data API: {}";
 
     public List<Contribution> findContribution(Integer repId, Boolean findLatestContribution) {
+        log.debug("Request to find contribution for repId: {} findLatestContribution: {} ", repId, findLatestContribution);
         List<Contribution> response = maatAPIClient.get(
                 new ParameterizedTypeReference<>() {
                 },
                 configuration.getMaatApi().getContributionEndpoints().getFindUrl() + "?findLatestContribution=" + findLatestContribution,
                 repId
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 
     public Contribution createContribution(CreateContributionRequest createContributionRequest) {
+        log.debug("Request to create contribution: {}", createContributionRequest);
         Contribution response = maatAPIClient.post(
                 createContributionRequest,
                 new ParameterizedTypeReference<>() {
@@ -47,51 +49,55 @@ public class MaatCourtDataService {
                 configuration.getMaatApi().getContributionEndpoints().getBaseUrl(),
                 Map.of()
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 
     public RepOrderDTO getRepOrderByRepId(Integer repId) {
+        log.info("Request to get rep order for repId: {}", repId);
         var response = maatAPIClient.get(
                 new ParameterizedTypeReference<RepOrderDTO>() {
                 },
                 configuration.getMaatApi().getContributionEndpoints().getGetRepOrderUrl(),
                 repId
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 
     public List<RepOrderCCOutcomeDTO> getRepOrderCCOutcomeByRepId(Integer repId) {
+        log.debug("Request to get rep order CC outcome for repId: {}", repId);
         List<RepOrderCCOutcomeDTO> response = maatAPIClient.get(
                 new ParameterizedTypeReference<>() {
                 },
                 configuration.getMaatApi().getRepOrderEndpoints().getFindOutcomeUrl(),
                 repId
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 
     public List<ContributionsSummaryDTO> getContributionsSummary(Integer repId) {
+        log.debug("Request to get contributions summary for repId: {}", repId);
         List<ContributionsSummaryDTO> response = maatAPIClient.get(
                 new ParameterizedTypeReference<>() {
                 },
                 configuration.getMaatApi().getContributionEndpoints().getSummaryUrl(),
                 repId
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 
     public ContributionCalcParametersDTO getContributionCalcParameters(String effectiveDate) {
+        log.debug("Request to get contribution calc parameters for effectiveDate: {}", effectiveDate);
         ContributionCalcParametersDTO response = maatAPIClient.get(
                 new ParameterizedTypeReference<>() {
                 },
                 configuration.getMaatApi().getContributionEndpoints().getContribsParametersUrl(),
                 effectiveDate
         );
-        log.info(RESPONSE_STRING, response);
+        log.debug(RESPONSE_STRING, response);
         return response;
     }
 }
