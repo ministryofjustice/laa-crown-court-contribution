e34dbb66284fcf145d611b6734813c250498f20e
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2025-05-13 17:57:13 +0100

    Replace stub-runner with spring-boot wiremock
    Removed the spring-cloud-starter-contract-stub-runner dependency due to
    transitive vulnerabilities and replaced it with the official Spring Boot
    WireMock integration.
    
    The project was only using stub-runner for the @AutoConfigureWireMock
    annotation. This has been replaced with @EnableWireMock and @InjectWireMock
    provided by the new WireMock Spring Boot module.
    
    Additional configuration and minor test changes were made to support the
    transition while retaining equivalent functionality.

--- crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/controller/ContributionControllerCalculateContributionIntegrationTest.java
+++ crown-court-contribution/src/test/java/uk/gov/justice/laa/crime/contribution/controller/ContributionControllerCalculateContributionIntegrationTest.java
@@ -1,18 +1,24 @@
 package uk.gov.justice.laa.crime.contribution.controller;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequestGivenContent;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -21,34 +27,26 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
 import uk.gov.justice.laa.crime.common.model.contribution.ApiCalculateContributionRequest;
 import uk.gov.justice.laa.crime.contribution.CrownCourtContributionApplication;
 import uk.gov.justice.laa.crime.contribution.config.CrownCourtContributionTestConfiguration;
 import uk.gov.justice.laa.crime.contribution.data.builder.TestModelDataBuilder;
 
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.UUID;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.justice.laa.crime.util.RequestBuilderUtils.buildRequestGivenContent;
-
+@EnableWireMock
 @DirtiesContext
+@AutoConfigureObservability
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import(CrownCourtContributionTestConfiguration.class)
 @SpringBootTest(classes = CrownCourtContributionApplication.class, webEnvironment = DEFINED_PORT)
-@AutoConfigureObservability
-@AutoConfigureWireMock(port = 9999)
 class ContributionControllerCalculateContributionIntegrationTest {
 
     private MockMvc mvc;
     private static final String ENDPOINT_URL = "/api/internal/v2/contribution/calculate";
 
-    @Autowired
-    private WireMockServer wiremock;
+    @InjectWireMock
+    private static WireMockServer wiremock;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -59,12 +57,13 @@ class ContributionControllerCalculateContributionIntegrationTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @AfterEach
-    void after() {
-        wiremock.resetAll();
+    @BeforeEach
+    void setup() throws JsonProcessingException {
+        stubForOAuth();
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .addFilter(springSecurityFilterChain).build();
     }
 
-    @BeforeAll
     void stubForOAuth() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> token = Map.of(
@@ -80,15 +79,8 @@ class ContributionControllerCalculateContributionIntegrationTest {
                                 .withBody(mapper.writeValueAsString(token))
                 )
         );
-
-        wiremock.start();
     }
 
-    @BeforeEach
-    public void setup() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
-                .addFilter(springSecurityFilterChain).build();
-    }
 
     @Test
     void givenAEmptyContent_whenCalculateContributionIsInvoked_thenFailsWithBadRequest() throws Exception {
