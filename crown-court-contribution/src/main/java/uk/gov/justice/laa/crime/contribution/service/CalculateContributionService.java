8d90e151a50ea05ac86f7c6c381ea89dd5d540d7
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2026-01-21 14:05:10 +0000

    Testing a fix

--- crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/CalculateContributionService.java
+++ crown-court-contribution/src/main/java/uk/gov/justice/laa/crime/contribution/service/CalculateContributionService.java
@@ -24,6 +24,7 @@ public class CalculateContributionService {
                     request.getUpliftedIncomePercent(),
                     request.getMinUpliftedMonthlyAmount());
             response.setMonthlyContributions(monthlyContributions);
+            response.setUpfrontContributions(BigDecimal.ZERO);
             response.setUpliftApplied(Constants.Y);
         } else {
             BigDecimal monthlyContributions = CalculateContributionUtil.calculateMonthlyContribution(
